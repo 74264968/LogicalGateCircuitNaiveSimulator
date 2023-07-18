a41f8fc312172e761b7dd28dd293ffe16169442a
understream <me@nowhere.com>
2023-07-18 16:41:05 +0800

    add edge detect

---
+++ core/prefabs/stateless.js
@@ -0,0 +1,26 @@
+//2023-07-18
+
+class EdgeDetector {
+  constructor( input, last, prefix ) {
+    this.network = [];
+    var not_input = new NotGate( prefix + "_ni", input );
+    var edge_detect = new AndGate( prefix + "_ed" );
+    edge_detect.inputs.push( not_input );
+    edge_detect.inputs.push( input );
+    this.output = new OrGate( prefix + "_o" );
+    this.output.inputs.push( edge_detect );
+    for( var i = 1 ; i <= last ; i++ ) {
+      var conn = new Connector( edge_detect, i, prefix + "_d" + i );
+      this.output.inputs.push( conn );
+      this.network.push( conn );
+    }
+    this.network.push( not_input );
+    this.network.push( edge_detect );
+    this.network.push( this.output );
+
+  }
+
+  get_output_endpoint() {
+    return this.output;
+  }
+}
