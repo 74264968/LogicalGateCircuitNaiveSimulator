f3b7655e6f22ee29fd6a790355071f2c67e0b4f5
understream <me@nowhere.com>
2023-10-24 10:01:21 +0800

    add display hex

--- visualization/dec-display.js
+++ visualization/dec-display.js
@@ -2,14 +2,14 @@
   2023-07-07
 */
 class DecimalDisplay {
-  constructor( signals_lsb2msb, signed, parent_dom_id, label_name ) {
+  constructor( signals_lsb2msb, signed, parent_dom_id, label_name, as_hex ) {
     var label = document.createElement("label");
     {
       label.innerHTML = label_name;
     }
     var textbox = document.createElement("input");
     {
-      textbox.setAttribute("type", "number");
+      //textbox.setAttribute("type", "number");
       textbox.setAttribute("readonly", "true");
     }
     var container = document.createElement("div");
@@ -23,12 +23,22 @@ class DecimalDisplay {
     this.signals = signals_lsb2msb;
     this.signed = signed;
     this.textbox = textbox;
+    this.as_hex = as_hex;
   }
 
   update( tick ) {
     var dec_v = 0;
     var base = 1;
-    if( this.signed && this.signals[this.signals.length-1].peek(tick) ) {
+    var hex = '';
+    var ALPHABET = '0123456789ABCDEF';
+    if( this.as_hex ) {
+      for( var i = 0 ; i < this.signals.length ; i+= 8 ) {
+        var values = parseInt(this.signals.slice(i,i+8).map( (x) => x.peek(tick) ).reverse().join(''),2);
+        hex += ALPHABET[Math.floor(values / 16)] + ALPHABET[values%16];
+      }
+      this.textbox.value = '0x' + hex + '';
+    }
+    else if( this.signed && this.signals[this.signals.length-1].peek(tick) ) {
       for( var i = 0 ; i < this.signals.length - 1; i++ ) {
         dec_v += (1 - this.signals[i].peek(tick)) * base;
         base *= 2;
